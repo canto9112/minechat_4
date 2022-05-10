db6e00162e3c043e206251decf3513be0ab87f56
filippov <cantoweb112@gmail.com>
2022-05-10 10:23:07 +0300

    Мелкие правки

--- listen-minechat.py
+++ listen-minechat.py
@@ -1,45 +1,46 @@
 import asyncio
 import datetime
-import argparse
-from environs import Env
 import logging
 
-log = logging.getLogger(__file__)
-handler = logging.FileHandler('logfile.log')
-log.addHandler(handler)
+import aiofiles
+from environs import Env
 
-def create_parser():
-    parser = argparse.ArgumentParser(description='Подключаемся к подпольному чату Minecraft')
-    parser.add_argument('--host', help='host', type=str)
-    parser.add_argument('--port', help='port', type=int)
-    parser.add_argument( '--history', help='history', type=str)
-    return parser
+from settings import create_parser
+
+log = logging.getLogger(__file__)
+
 
 async def start_listening(host, port, history):
+
     while True:
-        reader, writer = await asyncio.open_connection(host, port)
-        date = datetime.datetime.now()
-        formatted_date = date.strftime("%d.%m.%Y %H:%M")
+        try:
+            reader, writer = await asyncio.open_connection(host, port)
+            date = datetime.datetime.now()
+            formatted_date = date.strftime("%d.%m.%Y %H:%M")
 
-        data = await reader.read(1000)
-        message = f"[{formatted_date}] {data.decode()}"
+            data = await reader.read(1000)
+            message = f"[{formatted_date}] {data.decode()}"
+            log.info(message)
 
-        log.info(message)
-        with open(history,"a") as f:
-            f.write(message)
+            async with aiofiles.open(history, mode="a") as file:
+                await file.write(message)
+        except TimeoutError:
+            log.error("Пропало соединение с сетью")
+
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+    logging.basicConfig(format='%(message)s',
                         level=logging.INFO)
     env = Env()
     env.read_env()
 
-    host = env("host")
-    port = env("port")
-    history = env("history")
+    host = env("HOST")
+    port = env("LISTEN_PORT")
+    history = env("HISTORY")
 
     parser = create_parser()
     args = parser.parse_args()
+
     if args.host and args.port and args.history:
         asyncio.run(start_listening(args.host, args.port, args.history))
     else:
--- send_message.py
+++ send_message.py
@@ -1,33 +1,84 @@
 import asyncio
-from environs import Env
+import json
 import logging
 
+from environs import Env
+
+from settings import create_parser
+
+log = logging.getLogger(__file__)
 
 
-async def start_listening(host, port):
+async def get_data(reader):
+    data = await reader.readline()
+    log.debug(f"Получили: {data.decode()}")
+    return data.decode()
+
+
+async def send_message(writer, message):
+    writer.write(f'{message}\n\n'.encode())
+    await writer.drain()
+
+
+async def authorise(reader, writer, token):
+    await get_data(reader)
+    await send_message(writer, token)
+    login_data = await get_data(reader)
+    return json.loads(login_data.encode())
+
+
+async def register(writer, reader, nick_name):
+    await get_data(reader)
+    writer.write(f'\n'.encode())
+    await writer.drain()
+    await get_data(reader)
+    writer.write(f'{nick_name}\n'.encode())
+    register_data = await get_data(reader)
+    response = json.loads(register_data.encode())
+    log.debug(json.loads(register_data.encode()))
+    writer.close()
+    return response
+
+
+async def submit_message(writer):
     while True:
-        reader, writer = await asyncio.open_connection(host, port)
-        data = await reader.read(1000)
-        message = f"[{data.decode()}"
-        print(message)
+        message = input("Введите сообщение: ")
+        await send_message(writer, message)
 
-        data = input('Введите сообзение: ')
-        writer.write(data.encode('utf-8'))
-        await writer.drain()
+
+async def start_server(host, port):
+    reader, writer = await asyncio.open_connection(host, port)
+    hello_message = input("Зарегистрироваться?")
+    say_yes = ["y", "Y", "yes", "Yes", "д", "Да", "Д"]
+
+    if hello_message in say_yes:
+        nick_name = input("Введите имя пользователя: ")
+        register_data = await register(writer, reader, nick_name)
+        if register_data:
+            reader, writer = await asyncio.open_connection(host, port)
+            await authorise(reader, writer, register_data['account_hash'])
+            await submit_message(writer)
+    else:
+        token = input("Введите свой токен: ")
+        login_data = await authorise(reader, writer, token)
+        if login_data:
+            await submit_message(writer)
+        else:
+            log.error(f"Неуспешная авторизация")
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logging.basicConfig(format='%(message)s', level=logging.WARNING)
     env = Env()
     env.read_env()
 
-    host = env("host")
-    send_port = env("send_port")
-    user_hash = env("user_hash")
+    host = env("HOST")
+    port_send = env("WRITE_PORT")
+    token = env("TOKEN")
 
-
-    asyncio.run(start_listening(host, send_port))
-
-
-
-
+    parser = create_parser()
+    args = parser.parse_args()
+    if args.host and args.port and args.history:
+        asyncio.run(start_server(args.host, args.port_send))
+    else:
+        asyncio.run(start_server(host, port_send))
